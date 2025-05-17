3391765615e664706d482f28899504f70e982fa6
satendra <Hanuma@email.com>
2025-05-16 20:33:06 -0700

    form tag.

---
+++ satendra1.js
@@ -0,0 +1,15 @@
+function sa(name){
+    console.log(name,"Hello world");
+}
+sa(90);
+
+function sum(a,b){
+    console.log("sum of two number:",a+b)
+}
+sum(67,90);
+
+function sum(a,b){
+    return a+b;
+}
+$d=sum(0.78,90);
+console.log($d)
